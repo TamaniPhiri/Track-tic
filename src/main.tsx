c254bdff11c111da212b6c447ab89076105d3da8
tamani phiri <tamanigabriel0@gmail.com>
2024-05-04 12:50:21 +0200

    c

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { RecoilRoot } from "recoil";
+import { Toaster } from "@/Components/ui/toaster";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RecoilRoot>
       <BrowserRouter>
+        <Toaster />
         <App />
       </BrowserRouter>
     </RecoilRoot>
