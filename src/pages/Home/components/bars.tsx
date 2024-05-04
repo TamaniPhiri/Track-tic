097d741ce851811b0a4dbb00f8a65e23325e49ec
tamani phiri <tamanigabriel0@gmail.com>
2024-05-04 12:19:37 +0200

    c

--- src/pages/Home/components/bars.tsx
+++ src/pages/Home/components/bars.tsx
@@ -1,11 +1,14 @@
 const Bars = () => {
   return (
-    <div className=" shadow-xl shadow-neutral-800/50 border my-6 items-end border-neutral-800 rounded-xl p-12 flex justify-between">
-      <h1>
-        <span className="font-bold">K </span>{" "}
-        <span className="text-4xl font-bold">765</span>
-      </h1>
-      <h2 className="text-gray-500">ZMW</h2>
+    <div className="relative inline-flex group w-full my-5">
+      <div className="absolute transitiona-all duration-1000 opacity-30 -inset-px bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl blur-3xl group-hover:opacity-50 group-hover:inset-0 group-hover:duration-200 animate-tilt"></div>
+      <div className="relative inline-flex shadow-xl shadow-neutral-800/50 border items-end border-neutral-800 rounded-xl p-12 bg-black w-full justify-between">
+        <h1>
+          <span className="font-bold">K </span>{" "}
+          <span className="text-4xl font-bold">765</span>
+        </h1>
+        <h2 className="text-gray-500">ZMW</h2>
+      </div>
     </div>
   );
 };
