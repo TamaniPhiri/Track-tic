5741b36412f11ce09b34ec1d5f76d5c1a05faec5
tamani phiri <tamanigabriel0@gmail.com>
2024-05-04 12:12:23 +0200

    c

--- src/Components/global/NavBar.tsx
+++ src/Components/global/NavBar.tsx
@@ -3,7 +3,7 @@ import AddExpense from "@/pages/Home/components/add-expense";
 const NavBar = () => {
   return (
     <nav className="fixed bottom-0 inset-x-0 px-4">
-      <div className="flex w-full items-center p-4 justify-between w-full bg-black bg-opacity-70 backdrop-blur-md">
+      <div className="flex w-full items-center p-4 justify-between bg-black bg-opacity-70 backdrop-blur-md">
         <div>1</div>
         <AddExpense />
         <div>1</div>
