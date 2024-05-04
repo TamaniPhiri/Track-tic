93cb0a5dfbf156c53c1c1f3d3835f3b21b14f01f
tamani phiri <tamanigabriel0@gmail.com>
2024-05-04 12:33:52 +0200

    c

--- src/pages/Home/components/add-expense.tsx
+++ src/pages/Home/components/add-expense.tsx
@@ -18,7 +18,24 @@ import {
 const AddExpense = () => {
   return (
     <Sheet>
-      <SheetTrigger>Open</SheetTrigger>
+      <SheetTrigger>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="28"
+          height="28"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          className="lucide lucide-circle-plus"
+        >
+          <circle cx="12" cy="12" r="10" />
+          <path d="M8 12h8" />
+          <path d="M12 8v8" />
+        </svg>
+      </SheetTrigger>
       <SheetContent
         side={"bottom"}
         className="min-h-[50vh] bg-black border-t-neutral-600"
