2faf4d9ea222818dfe9b9fe4620f90129f177206
tamani phiri <tamanigabriel0@gmail.com>
2024-05-04 12:34:01 +0200

    c

--- src/pages/Home/components/add-expense.tsx
+++ src/pages/Home/components/add-expense.tsx
@@ -21,8 +21,8 @@ const AddExpense = () => {
       <SheetTrigger>
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          width="28"
-          height="28"
+          width="32"
+          height="32"
           viewBox="0 0 24 24"
           fill="none"
           stroke="currentColor"
