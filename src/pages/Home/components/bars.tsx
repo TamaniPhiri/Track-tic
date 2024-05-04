15ec4823f43c4b07f46f64c7ad2091e25719c993
tamani phiri <tamanigabriel0@gmail.com>
2024-05-04 11:59:37 +0200

    c

--- src/pages/Home/components/bars.tsx
+++ src/pages/Home/components/bars.tsx
@@ -1,6 +1,6 @@
 const Bars = () => {
   return (
-    <div className="border p-12 flex justify-between">
+    <div className=" shadow-xl shadow-neutral-800 border my-6 border-neutral-800 rounded-xl p-12 flex justify-between">
       <h1> K 765</h1>
       <h1>ZMW</h1>
     </div>
