29211eef8ff6b0e1584fa302f4c28fcff9361186
tamani phiri <tamanigabriel0@gmail.com>
2024-05-04 12:01:56 +0200

    c

--- src/pages/Home/components/bars.tsx
+++ src/pages/Home/components/bars.tsx
@@ -1,8 +1,11 @@
 const Bars = () => {
   return (
-    <div className=" shadow-xl shadow-neutral-800 border my-6 border-neutral-800 rounded-xl p-12 flex justify-between">
-      <h1> K 765</h1>
-      <h1>ZMW</h1>
+    <div className=" shadow-xl shadow-neutral-800 border my-6 items-end border-neutral-800 rounded-xl p-12 flex justify-between">
+      <h1>
+        <span className="font-bold">K </span>{" "}
+        <span className="text-4xl font-bold">765</span>
+      </h1>
+      <h2 className="text-gray-500">ZMW</h2>
     </div>
   );
 };
